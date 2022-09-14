4e17b998266088e48a538e6f13611dbc52eb9cad
Jesica-akeyo <99292017+Jesica-akeyo@users.noreply.github.com>
2022-09-14 15:18:27 +0300

    states and cities

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -15,43 +15,43 @@ from sqlalchemy.orm import sessionmaker
 from os import environ
 
 
-classes = {"Amenity": Amenity, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
-
-
 class DBStorage:
     """ Storage for database with SQL Alchemy and MySQL """
     __engine = None
     __session = None
 
     def __init__(self):
-        """Instantiate a DBStorage object"""
-        sqlUSER = getenv("HBNB_MYSQL_USER")
-        sqlPWD = getenv("HBNB_MYSQL_PWD")
-        sqlDB = getenv("HBNB_MYSQL_DB")
-        sqlHOST = getenv("HBNB_MYSQL_HOST")
-        sqlENV = getenv("HBNB_ENV")
+        """ Constructor """
+
+        sqlUSER = environ.get('HBNB_MYSQL_USER')
+        sqlPWD = environ.get('HBNB_MYSQL_PWD')
+        sqlHOST = environ.get('HBNB_MYSQL_HOST')
+        sqlDB = environ.get('HBNB_MYSQL_DB')
+        sqlENV = environ.get('HBNB_ENV')
 
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
                                       format(sqlUSER, sqlPWD, sqlHOST, sqlDB),
-                                      pool_pre_ping=True))
+                                      pool_pre_ping=True)
 
         if sqlENV == "test":
-            Base.metadata.drop_all(bind = self.__engine)
+            Base.metadata.drop_all(bind=self.__engine)
 
     def all(self, cls=None):
-        """query on the current database session
-        returns a dictionary of __objects
         """
+        Query on the current database session (self.__session)
+        all objects depending of the class name (argument cls)
+        """
+
         session = self.__session
         dic = {}
         if not cls:
             tables = [User, State, City, Amenity, Place, Review]
+
         else:
             if type(cls) == str:
-                cls = eval(cls)
+                cls = eval(csl)
 
-                tables = [cls]
+            tables = [cls]
 
         for t in tables:
             query = session.query(t).all()
@@ -59,32 +59,37 @@ class DBStorage:
             for rows in query:
                 key = "{}.{}".format(type(rows).__name__, rows.id)
                 dic[key] = rows
-        return (dic)
+
+        return dic
 
     def new(self, obj):
-        """add the object to the current database session"""
-        self.__session.add(obj)
+        """ add the object to the current database session """
+        if obj:
+            self.__session.add(obj)
 
     def save(self):
-        """ commit all the changes of the current database session"""
+        """ commit all changes of the current database session """
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete from the current database session obj if not None"""
+        """ delete from module import symbol
+        the current database session obj if not None """
         if obj:
-            self.session.delete(obj)
+            self.__session.delete(obj)
 
     def reload(self):
-        """reloads data from the database
+        """
         creates all tables in the database
-        create the current database session
+        creates the current database session
         """
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine, 
+        session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
 
     def close(self):
-        """closes session"""
+        """
+        Closes Session
+        """
         self.__session.close()
